115c8999d2579e7fdaea8686a8b632760476f407
daiab1 <71070793+daiab1@users.noreply.github.com>
2020-12-10 00:09:22 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    
+$('.productSlider').slick({
+  centerMode: false,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+});
